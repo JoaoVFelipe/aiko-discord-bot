94ec9fe7cd654cfc6c8c276709db65be618350a8
João Vitor Felipe <joaovitor.felipesantos@gmail.com>
2025-08-05 16:37:07 -0300

    fix: atualiza versão dos pacotes e reativa o projeto

--- engine/music_player.py
+++ engine/music_player.py
@@ -1,37 +1,43 @@
+import os
 import discord
-import youtube_dl
+import yt_dlp as youtube_dl
 import asyncio
 import validators
-import re, requests, subprocess, urllib.parse, urllib.request
+import re, urllib.parse, urllib.request
+
+import imageio_ffmpeg as ffmpeg
 
 from engine import discord_actions, music_player
 
-# General variables
+# --------- Configurações globais ---------
 queue = {}
 queue_list_limit = 10
 
-# Code to play music from youtube 
+# yt-dlp options
 ytdl_format_options = {
     'format': 'bestaudio/best',
     'outtmpl': 'downloads/%(extractor)s-%(id)s-%(title)s.%(ext)s',
     'restrictfilenames': True,
-    'noplaylist': True,
-    'extract_flat': True,
+    'noplaylist': False,  # permite playlist (tratada no código)
     'nocheckcertificate': True,
     'ignoreerrors': False,
     'logtostderr': False,
     'quiet': True,
     'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0'  # ipv6 addresses cause issues sometimes
+    'default_search': 'ytsearch',
+    'source_address': '0.0.0.0'
 }
 
+# FFMPEG_PATH = os.path.join(os.path.dirname(__file__), '..', 'assets', 'ffmpeg', 'bin', 'ffmpeg.exe')
+FFMPEG_PATH = ffmpeg.get_ffmpeg_exe()
+
 ffmpeg_options = {
-    'options': '-vn',
-    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -nostdin',
+    'options': '-f s16le -ar 48000 -ac 2 -vn -loglevel panic'
 }
 
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
 
 class YTDLSource(discord.PCMVolumeTransformer):
     def __init__(self, source, *, data, volume=0.5):
@@ -41,117 +47,151 @@ class YTDLSource(discord.PCMVolumeTransformer):
         self.url = data.get('url')
 
     @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False, serverQueue=None, message=None):
-        data = ytdl.extract_info(url, download=False)
-        if not data: 
+    async def from_url(cls, url, *, loop=None, stream=True, serverQueue=None, message=None):
+        """Extrai informações do vídeo ou playlist e retorna um player pronto"""
+        data = await asyncio.to_thread(lambda: ytdl.extract_info(url, download=False))
+        if not data:
             return False
-        else:
-            if 'entries' in data:
-                return await music_player.manage_playlist(playlist=data['entries'], message=message, serverQueue=serverQueue)
-            else:
-                filename = data['url']
-                return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
+        # Playlist → envia para função de gerenciamento
+        if 'entries' in data and data.get('_type') == 'playlist':
+            return await music_player.manage_playlist(
+                playlist=data['entries'],
+                message=message,
+                serverQueue=serverQueue
+            )
+
+        # Stream direto
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, executable=FFMPEG_PATH, **ffmpeg_options), data=data)
+
+
+# --------- Comandos principais ----------
 async def execute(message):
     serverQueue = queue.get(message.guild.id)
 
-    if not serverQueue:
-        song_url = get_youtube_url(message)
-        if not song_url:
-            return await discord_actions.send_message(channel=message.channel, message_text="Não fui capaz de encontrar a faixa solicitada :(")
+    if serverQueue and not serverQueue['playing'] and serverQueue['connection'].is_paused():
+        await execute_resume(message)
+        return await discord_actions.send_message(
+            channel=message.channel,
+            message_text="Música retomada!"
+        )
 
+    song_url = get_youtube_url(message)
+    if not song_url:
+        return await discord_actions.send_message(
+            channel=message.channel,
+            message_text="Não fui capaz de encontrar a faixa solicitada :("
+        )
+
+    if not serverQueue:
+        # Cria nova fila
         queueContruct = {
             "text_channel": message.channel,
             "voice_channel": "",
             "connection": "",
             "songs": [],
-            "playing": True 
+            "playing": True
         }
 
-        # Connect to user voice channel
+        # Conecta ao canal de voz
         connection = await discord_actions.connect_voice_channel(message)
-
         if not connection or not connection.channel:
-            await discord_actions.send_message(message.channel, 'Você precisa estar em um canal de voz para executar este comando!')
-            return False
-        else:
-            queueContruct['connection'] = connection
-            queueContruct['voice_channel'] = connection.channel
-            queue[message.guild.id] = queueContruct
-            player = await play(message.guild, song_url, message)
-            if player:
-                return await discord_actions.send_message(channel=message.channel, message_text="Tocando agora: {}".format(player.title))
-            else:
-                return
+            return await discord_actions.send_message(
+                message.channel,
+                'Você precisa estar em um canal de voz para executar este comando!'
+            )
 
-    elif serverQueue['playing']:
-        song_url = get_youtube_url(message)
-        if not song_url:
-            return await discord_actions.send_message(channel=message.channel, message_text="Não fui capaz de encontrar a faixa solicitada :(")
-        if len(serverQueue['songs']) == 0 and not await check_is_playing(serverQueue):
-            player = await play(message.guild, song_url, message)
-            return await discord_actions.send_message(channel=message.channel, message_text="Tocando agora: {}".format(player.title))
-        else: 
-            player = await YTDLSource.from_url(song_url, stream=True, serverQueue=serverQueue, message=message)
-            if player:
-                serverQueue['songs'].append({'url': song_url, 'title': player.title})
-                return await discord_actions.send_message(channel=message.channel, message_text='{} foi adicionado a fila!'.format(player.title), message_description='Total de músicas na fila: {}'.format(len(serverQueue['songs'])))
-    else: 
-        await execute_resume(message)
+        queueContruct['connection'] = connection
+        queueContruct['voice_channel'] = connection.channel
+        queue[message.guild.id] = queueContruct
 
-async def execute_pause(message): 
+        player = await play(message.guild, song_url, message)
+        if player:
+            return await discord_actions.send_message(
+                channel=message.channel,
+                message_text=f"Tocando agora: {player.title}"
+            )
+    else:
+        # Já existe fila em execução
+        player = await YTDLSource.from_url(song_url, stream=True, serverQueue=serverQueue, message=message)
+        if player:
+            serverQueue['songs'].append({'url': song_url, 'title': player.title})
+            return await discord_actions.send_message(
+                channel=message.channel,
+                message_text=f'{player.title} foi adicionado à fila!',
+                message_description=f'Total de músicas na fila: {len(serverQueue["songs"])}'
+            )
+
+
+async def execute_pause(message):
     if not message.author.voice.channel:
-        return await discord_actions.send_message(channel=message.channel, message_text="Você precisa estar em um canal de voz para pausar a música!")
+        return await discord_actions.send_message(
+            channel=message.channel,
+            message_text="Você precisa estar em um canal de voz para pausar a música!"
+        )
 
     serverQueue = queue.get(message.guild.id)
-
     if not serverQueue:
         return await discord_actions.send_message(channel=message.channel, message_text="Não há músicas para pausar!")
 
     serverQueue['playing'] = False
     serverQueue['connection'].pause()
 
-async def execute_resume(message): 
+
+async def execute_resume(message):
     if not message.author.voice.channel:
-        return await discord_actions.send_message(channel=message.channel, message_text="Você precisa estar em um canal de voz para retomar a música!")
+        return await discord_actions.send_message(
+            channel=message.channel,
+            message_text="Você precisa estar em um canal de voz para retomar a música!"
+        )
 
     serverQueue = queue.get(message.guild.id)
-
     if not serverQueue:
-        return await discord_actions.send_message(channel=message.channel, message_text="Não há músicas para retomar!")
+        return await discord_actions.send_message(
+            channel=message.channel,
+            message_text="Não há músicas para retomar!"
+        )
 
-    serverQueue['playing'] = True
-    serverQueue['connection'].resume()
+    if serverQueue['connection'].is_paused():
+        serverQueue['connection'].resume()
+        serverQueue['playing'] = True
+        return await discord_actions.send_message(
+            channel=message.channel,
+            message_text="Música retomada!"
+        )
 
-async def execute_skip(message): 
+
+async def execute_skip(message):
     if not message.author.voice.channel:
-        return await discord_actions.send_message(channel=message.channel, message_text="Você precisa estar em um canal de voz para pular alguma música!")
+        return await discord_actions.send_message(
+            channel=message.channel,
+            message_text="Você precisa estar em um canal de voz para pular alguma música!"
+        )
 
     serverQueue = queue.get(message.guild.id)
-
     if not serverQueue:
         return await discord_actions.send_message(channel=message.channel, message_text="Não há músicas para pular!")
 
-    if len(serverQueue['songs']):
-        serverQueue['connection'].stop()
-        # await play_next(serverQueue, message.guild, message)
-    else:
-        serverQueue['connection'].stop()
+    serverQueue['connection'].stop()
+
 
 async def execute_stop(message):
     if not message.author.voice.channel:
-        return await discord_actions.send_message(channel=message.channel, message_text="Você precisa estar em um canal de voz para parar a música!")
+        return await discord_actions.send_message(
+            channel=message.channel,
+            message_text="Você precisa estar em um canal de voz para parar a música!"
+        )
 
     serverQueue = queue.get(message.guild.id)
-
     if not serverQueue:
         return await discord_actions.send_message(channel=message.channel, message_text="Não há músicas para parar!")
-        
+
     serverQueue['songs'] = []
     serverQueue['connection'].stop()
     await serverQueue['connection'].disconnect()
-    queue.pop(message.guild.id)
-    return
+    queue.pop(message.guild.id, None)
+
 
 async def execute_jump_to(message):
     if not message.author.voice.channel:
@@ -220,87 +260,76 @@ async def play(guild, song, message):
     if not serverQueue:
         return
 
-    if not isinstance(song, str):
-        to_play = song['url']
-        player = await YTDLSource.from_url(to_play, stream=True, serverQueue=serverQueue, message=message)
-        serverQueue['playing_now'] = {'url':song['url'], 'title':song['title']}
-        queue[guild.id] = serverQueue
-    else:
-        player = await YTDLSource.from_url(song, stream=True, serverQueue=serverQueue, message=message)
-        if player:
-            serverQueue['playing_now'] = {'url':song, 'title':player.title}
-            queue[guild.id] = serverQueue
+    player = await YTDLSource.from_url(song, stream=True, serverQueue=serverQueue, message=message)
     if player:
-        serverQueue['connection'].play(player, after= lambda e: asyncio.run(play_next(serverQueue, guild, message)))
+        serverQueue['playing_now'] = {'url': song, 'title': player.title}
+        queue[guild.id] = serverQueue
+        serverQueue['connection'].play(
+            player,
+            after=lambda e: asyncio.run(play_next(serverQueue, guild, message))
+        )
         return player
-    else:
-        return False
+    return False
+
 
 async def play_next(serverQueue, guild, message):
     if len(serverQueue['songs']):
-        to_play = serverQueue['songs'][0]
-        await play(guild, to_play, message)
-        serverQueue['songs'].pop(0)
+        to_play = serverQueue['songs'].pop(0)
+        await play(guild, to_play['url'], message)
     else:
         if serverQueue and serverQueue['connection']:
             serverQueue['connection'].stop()
-        return
+
 
 async def check_is_playing(serverQueue):
-    if serverQueue and serverQueue['connection']:
-        return serverQueue['connection'].is_playing() or serverQueue['connection'].is_paused()
-    else:
-        return False
+    return serverQueue and serverQueue['connection'] and (
+        serverQueue['connection'].is_playing() or serverQueue['connection'].is_paused()
+    )
+
 
 async def manage_playlist(playlist, message, serverQueue):
     total_queue = 0
     for item in playlist:
         serverQueue['songs'].append({'url': item['url'], 'title': item['title']})
-        total_queue = total_queue + 1
+        total_queue += 1
 
-    await discord_actions.send_message(channel=message.channel, message_text='{} músicas adicionadas a fila!'.format(total_queue), message_description='Total de músicas na fila: {}'.format(len(serverQueue['songs'])))
+    await discord_actions.send_message(
+        channel=message.channel,
+        message_text=f'{total_queue} músicas adicionadas à fila!',
+        message_description=f'Total de músicas na fila: {len(serverQueue["songs"])}'
+    )
     if not await check_is_playing(serverQueue):
         return await music_player.play_next(serverQueue=serverQueue, message=message, guild=message.guild)
-    else:
-        return
+
 
 def get_youtube_url(message):
-    # Remove command word
     search_string_array = message.content.split(' ')
     search_string_array.pop(0)
     search_string = ' '.join(search_string_array)
 
-    if validators.url(search_string) is True:
+    if validators.url(search_string):
         return search_string
-    else:
-        search_string = search_string.lower()
-        # Format the search term
-        query_string = urllib.parse.urlencode({"search_query": search_string})
-        # Format the search url on youtube
-        format_url = urllib.request.urlopen("https://www.youtube.com/results?" + query_string)
-        # Find all the video results
-        search_results = re.findall(r"watch\?v=(\S{11})", format_url.read().decode())
 
-        if len(search_results):
-            # Get the first result
-            clip_url = "https://www.youtube.com/watch?v=" + "{}".format(search_results[0])
-            return clip_url
-        else:
-            return False
+    query_string = urllib.parse.urlencode({"search_query": search_string.lower()})
+    format_url = urllib.request.urlopen(f"https://www.youtube.com/results?{query_string}")
+    search_results = re.findall(r"watch\?v=(\S{11})", format_url.read().decode())
+    return f"https://www.youtube.com/watch?v={search_results[0]}" if search_results else False
+
 
 async def check_inactivity_queues():
     print("Checking for inactivity...")
     try:
         for key, instance in list(queue.items()):
-                serverQueue = queue.get(key)
-
-                if not await check_is_playing(serverQueue):
-                    print("Inactivity found! Disconnecting Id:", key)
-
-                    await discord_actions.send_message(channel=instance['text_channel'], message_text='Hey, alguém aí?', message_description='Estou desconectando devido a inatividade. Qualquer coisa estou por aqui, só chamar ;)')
-                    serverQueue['connection'].stop()
-                    await serverQueue['connection'].disconnect()
-                    queue.pop(key)
-                    print("Inactive instance disconected!")
-    except:
-        print("Erro ao tentar desconectar a instância do bot inativa.")
+            if not await check_is_playing(instance):
+                print(f"Inactivity found! Disconnecting Id: {key}")
+                await discord_actions.send_message(
+                    channel=instance['text_channel'],
+                    message_text='Hey, alguém aí?',
+                    message_description='Estou desconectando devido a inatividade. Qualquer coisa estou por aqui, só chamar ;)'
+                )
+                instance['connection'].stop()
+                await instance['connection'].disconnect()
+                queue.pop(key)
+                print("Inactive instance disconnected!")
+    except Exception as e:
+        print("Erro ao tentar desconectar a instância do bot inativa.", e)
--- main.py
+++ main.py
@@ -1,68 +1,105 @@
 import os
-import discord
 import asyncio
+import logging
 
+import signal
+import sys
+
+import discord
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-
 from dotenv import load_dotenv
+
 from engine import music_player, discord_actions, general
 
+# Configura logging para melhor depuração
+logging.basicConfig(level=logging.INFO)
+
+# Carrega variáveis de ambiente
 load_dotenv()
+TOKEN = os.getenv('BOT_TOKEN')
 
-# Connects to discord
-client = discord.Client()
-token = os.getenv('BOT_TOKEN')
+# Configura intents necessários
+intents = discord.Intents.default()
+intents.message_content = True
 
-# Registered events
+# Cria cliente do Discord
+client = discord.Client(intents=intents)
+
+# Scheduler para tarefas agendadas
+scheduler = AsyncIOScheduler()
+
+async def timed_job():
+    """Função executada periodicamente pelo scheduler"""
+    result = await music_player.check_inactivity_queues()
+    print(result)
+
+# ----------------- EVENTOS -----------------
 @client.event
 async def on_ready():
-    print('Aiko bot is ready to start!')
+    print(f"Aiko bot conectado como {client.user}")
+    # Inicia scheduler quando o bot estiver pronto
+    if not scheduler.running:
+        scheduler.add_job(timed_job, "interval", minutes=15)
+        scheduler.start()
+        print("Scheduler iniciado.")
 
 @client.event
-async def on_message(message):
-    if message.author == client.user or message.author.bot:
+async def on_message(message: discord.Message):
+    # Ignora mensagens do próprio bot
+    if message.author.bot:
         return
 
-    if message.content.startswith('!test'):
-        await discord_actions.send_message(channel=message.channel, message_text='I am connected and working!')
-        return
+    content = message.content.strip()
 
-    if message.content.startswith('!help'):
+    # ------------- Comandos ---------------
+    if content.startswith('!test'):
+        await discord_actions.send_message(
+            channel=message.channel, 
+            message_text='I am connected and working!'
+        )
+
+    elif content.startswith('!help'):
         await general.execute_help(message)
-        return
-
-    ############ MUSIC COMMANDS ##############
-    if message.content.startswith('!play'):
-        await music_player.execute(message)
-        return
-
-    if message.content.startswith('!pause'):
-        await music_player.execute_pause(message)
-        return
-
-    if message.content.startswith('!next'):
-        await music_player.execute_skip(message)
-        return
-
-    if message.content.startswith('!stop'):
-        await music_player.execute_stop(message)
-        return
         
-    if message.content.startswith('!queue'):
+    # ----------- Music player commands --------------
+    elif content.startswith('!play'):
+        await music_player.execute(message)
+    
+    elif content.startswith('!resume'):
+        await music_player.execute_resume(message)
+
+    elif content.startswith('!pause'):
+        await music_player.execute_pause(message)
+
+    elif content.startswith('!next'):
+        await music_player.execute_skip(message)
+
+    elif content.startswith('!stop'):
+        await music_player.execute_stop(message)
+
+    elif content.startswith('!queue'):
         await music_player.execute_list_queue(message)
-        return
 
-    if message.content.startswith('!jump_to'):
+    elif content.startswith('!jump_to'):
         await music_player.execute_jump_to(message)
-        return
 
-### Scheduler Functions
-async def timed_job():
-    print(await music_player.check_inactivity_queues())
+    # # ------------ AI commands ------------------
+    # if message.content.startswith('!aiko'):
+    #    await ai_chat.execute_ai_command(message)
+# ----------------- EXECUÇÃO -----------------
+if __name__ == "__main__":
+    if not TOKEN:
+        raise ValueError("O token do bot não foi definido no .env (BOT_TOKEN).")
+    print("Iniciando bot...")
+    client.run(TOKEN)
 
-scheduler = AsyncIOScheduler()
-scheduler.add_job(timed_job, "interval", minutes=15)
+def shutdown_handler(sig, frame):
+    print("Encerrando bot...")
+    # Desconecta do Discord
+    loop = asyncio.get_event_loop()
+    loop.create_task(client.close())
+    loop.stop()
+    sys.exit(0)
 
-scheduler.start()
-print("Scheduled started")
-client.run(token)
+signal.signal(signal.SIGINT, shutdown_handler)
+signal.signal(signal.SIGTERM, shutdown_handler)
