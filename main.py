2c209f650140a296a0aeb897731bfcce7dc82fc9
João Vitor Felipe <joaovitor.felipesantos@gmail.com>
2025-08-11 20:14:27 -0300

    fix: atualiza help e prefix

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.voice_states = True 
 
-bot = commands.Bot(command_prefix="!#", intents=intents, help_command=None)
+bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
 
 # ----------------- SCHEDULER -----------------
 ## Funções para as features que precisam rodar de tempos em tempos
